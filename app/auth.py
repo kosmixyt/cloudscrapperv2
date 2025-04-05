833e444f30433c00366f5b154f19100b58d98ca4
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2025-04-05 17:12:16 +0200

    fix jwt

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 from typing import Optional
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
 from passlib.context import CryptContext
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
